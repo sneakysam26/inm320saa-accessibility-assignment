9e8cac0b7965c52e3f4137c74d9cfe8ba474b173
sneakysam26 <samhardy06@gmail.com>
2026-03-01 13:28:10 -0500

    Adding Project Files
    adding project files to repo folder

---
+++ public/assets/js/main.js
@@ -0,0 +1,8 @@
+const checkbox_wrapper = document.querySelector( 'form .flexbox' );
+const checkbox = document.querySelector( 'form .flexbox span' );
+
+checkbox_wrapper.addEventListener("click", function(){
+    checkbox.classList.toggle( "checked" );
+});
+
+document.querySelector( '#footer-copyright' ).innerHTML = `&copy; ${new Date().getFullYear()} Dreamla - All Rights Reserved.`;
