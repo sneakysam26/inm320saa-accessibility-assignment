4303214585d340f447d2fb07db3c49bcbe9af659
sneakysam26 <samhardy06@gmail.com>
2026-04-12 21:41:56 -0400

    Resubmission
    Changed Aria Attributes for resubmission

--- public/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const checkbox_wrapper = document.querySelector( 'form .flexbox' );
-const checkbox = document.querySelector( 'form .flexbox span' );
-
-checkbox_wrapper.addEventListener("click", function(){
-    checkbox.classList.toggle( "checked" );
-});
-
-document.querySelector( '#footer-copyright' ).innerHTML = `&copy; ${new Date().getFullYear()} Dreamla - All Rights Reserved.`;
